fdc444e6c2ce0a2727b49cfcbdc1a92aae7fcf35
Shlok <shlok7869@gmail.com>
2025-10-02 19:19:06 +0530

    [shlok] add print in main method

--- src/main/java/com/example/HospitalManagmentSystemProject/HospitalManagmentSystemProjectApplication.java
+++ src/main/java/com/example/HospitalManagmentSystemProject/HospitalManagmentSystemProjectApplication.java
@@ -10,6 +10,7 @@ public class HospitalManagmentSystemProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagmentSystemProjectApplication.class, args);
+		System.out.println("Hospital");
 	}
 
 }
